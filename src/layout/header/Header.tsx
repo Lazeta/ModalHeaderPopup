87933a1ef6ef1c450b4cc55a56d5a070218daa12
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-03-06 17:09:29 +0300

    feat: added modal menu and onclick hendler for burger

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,42 +1,82 @@
-import React, { useState } from 'react';
-import styled from "styled-components"
+import React, { useState } from "react";
+import styled from "styled-components";
+
+type menuProps = {
+    isMenuOpen: boolean;
+};
 
 export const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <StyledHeaderBody>
             <StyledHeader>
-                <menu>
-
-                </menu>
-                <burger>
-                    
-                </burger>
+                <Menu isMenuOpen={isMenuOpen}>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </Menu>
+                <Burger onClick={toggleMenu}>
+                    <Line></Line>
+                    <Line></Line>
+                    <Line></Line>
+                </Burger>
             </StyledHeader>
         </StyledHeaderBody>
-    )
-}
-
+    );
+};
 
 const StyledHeaderBody = styled.div`
-    background-color: #3d3c3c;
-    width: 100%;
-    height: 100vh; 
-    overflow-y: hidden;
-`
+  background-color: #3d3c3c;
+  width: 100%;
+  height: 100vh;
+  overflow-y: hidden;
+`;
 
 const StyledHeader = styled.header`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    height: 100%;
-    padding: 20px;
+  display: flex;
+  /* align-items: center; */
+  justify-content: space-between;
+  height: 100%;
+  padding: 20px;
 
-`
+`;
 
-const menu = styled.nav`
-    
-`
+const Menu = styled.nav<menuProps>`
+  display: ${({ isMenuOpen }) => (isMenuOpen ? "flex" : "none")};
+  align-items: center;
+  margin: 0 auto;
+  z-index: 99;
+  ul {
+    list-style-type: none;
 
-const burger = styled.div`
-    
+    li {
+      color: #f0e2e2;
+      font-size: 2rem;
+      padding: 20px;
+    }
+  }
+`;
+
+const Burger = styled.div`
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+  position: fixed;
+  top: 40px;
+  right: 40px;
+  z-index: 100;
+`;
+
+const Line = styled.div`
+    width: 30px;
+    height: 3px;
+    background-color: #eeffee;
+    margin-bottom: 6px;
 `
